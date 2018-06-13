427047ad418519c486aad5ac246def381b5eecac
NolanHughes <Nhughes987@gmail.com>
2018-06-12 23:56:00 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-// Code Goes Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Order from './components/Order';
+
+var date = new Date();
+var hours = date.getHours();
+var minutes = date.getMinutes();
+var seconds = date.getSeconds();
+
+// Will display time in 10:30:23 format
+var formattedTime = hours + ':' + minutes + ':' + seconds;
+
+ReactDOM.render(
+  <Order size="small" cone={false} scoops={["rocky road", "cookies and cream"]} orderInfo={{customerName: "Jessie", orderedAt: formattedTime}} />,
+  document.getElementById('global')
+);
